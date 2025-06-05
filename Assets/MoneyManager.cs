2faabcb71375ba2c840da3c4b326c3299004881a
juligric <49377944@est.ort.edu.ar>
2025-06-05 11:57:41 -0300

    .

---
+++ Assets/MoneyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+    public UIMANAGER uiMANAGER;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   public void UpdateMoney(float moneyToAdd)
+    {
+        if(money + moneyToAdd < 0)
+        {
+            Debug.Log("no te alcanza");
+            return;
+        }
+        money += moneyToAdd;
+    }
+}
---
+++ Assets/mernacansiaScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mernacansiaScript : MonoBehaviour
+{
+    public float cost;
+    public MoneyManager money
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
